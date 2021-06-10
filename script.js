4ef7a0a5d1b869babe9b1bfb14a472ed6f449446
Tymee Kong <tymeekong562@gmail.com>
2021-06-09 20:24:50 -0700

    Added displayForm, clearFields, and added a new button

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let myLibrary = [];
 const titleForm = document.querySelector("#booktitle");
 const authorForm = document.querySelector("#author");
 const pageForm = document.querySelector("#page");
-const newBookButton = document.querySelector("#new-book");
-const divDisplay = document.querySelector("#book-display");
+const displayFormButton = document.querySelector("#display-form");
+const formPopUpDiv = document.querySelector("#form-popup");
+const saveButton = document.querySelector("#save-book");
+let formIsDisplayed = false;
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,12 +17,47 @@ function Book(title, author, pages, read) {
     }
 }
 
+function displayForm() {
+    if (!formIsDisplayed) {
+        displayFormButton.remove();
+        formPopUpDiv.style.display = "block";
+        formIsDisplayed = true;
+    }
+    else {
+        formPopUpDiv.style.display = "none";
+        const newBookDiv = document.querySelector("#button-display");
+        newBookDiv.appendChild(displayFormButton);
+        formIsDisplayed = false;
+    }    
+}
+
+
+
+function clearFields() {
+    titleForm.value = "";
+    authorForm.value = "";
+    pageForm.value = "";
+}
+
 function addBookToLibrary() {
+    if (!titleForm.value || !authorForm.value || !pageForm.value) {
+        alert("Please enter a value");
+    }
+    
     const book = new Book();
     book.title = titleForm.value;
     book.author = authorForm.value;
     book.pages = pageForm.value;
     myLibrary.push(book);
+    console.log(myLibrary);
+    displayForm();
+    clearFields();
 }
 
-newBookButton.addEventListener("click", addBookToLibrary);
+function clickSaveButton() {
+    addBookToLibrary();
+    
+}
+
+displayFormButton.addEventListener("click", displayForm);
+saveButton.addEventListener("click", clickSaveButton);
