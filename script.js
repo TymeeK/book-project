d70fb728eca7c13cd87d90ac4b8bf278ea0b2304
Tymee Kong <tymeekong562@gmail.com>
2021-06-11 11:21:38 -0700

    Added grid with black borders around it to display cards

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const pageForm = document.querySelector("#page");
 const displayFormButton = document.querySelector("#display-form");
 const formPopUpDiv = document.querySelector("#form-popup");
 const saveButton = document.querySelector("#save-book");
+const checkBox = document.querySelector("#check-read");
 let formIsDisplayed = false;
 
 function Book(title, author, pages, read) {
@@ -32,7 +33,6 @@ function displayForm() {
 }
 
 
-
 function clearFields() {
     titleForm.value = "";
     authorForm.value = "";
@@ -41,17 +41,40 @@ function clearFields() {
 
 function addBookToLibrary() {
     if (!titleForm.value || !authorForm.value || !pageForm.value) {
-        alert("Please enter a value");
+        alert("Please enter a value for all fields");
+        return;
+    }
+    if (!parseInt(pageForm.value)) {
+        alert("Please enter a number in the pages field");
+        return;
     }
     
     const book = new Book();
     book.title = titleForm.value;
     book.author = authorForm.value;
     book.pages = pageForm.value;
+    book.read = checkBox.checked;
     myLibrary.push(book);
     console.log(myLibrary);
     displayForm();
     clearFields();
+}
+
+function addBookCard() {
+    const bookDisplayDiv = document.querySelector("#book-display");
+    bookDisplayDiv.style.cssText = 
+        `grid-template-columns: repeat(6, 1fr);` +
+        `grid-template-rows: repeat(6, 1fr);`;
+    
+    for (let i = 0; i < 36; i++) {
+        const cardDisplay = document.createElement("div");
+        cardDisplay.className = "grid-div";
+        cardDisplay.innerText = "Hello";
+        cardDisplay.style.cssText = `border: 1px solid black;`;
+        bookDisplayDiv.appendChild(cardDisplay);
+    }
+   
+    
 }
 
 function clickSaveButton() {
@@ -59,5 +82,8 @@ function clickSaveButton() {
     
 }
 
+
 displayFormButton.addEventListener("click", displayForm);
 saveButton.addEventListener("click", clickSaveButton);
+
+addBookCard();
